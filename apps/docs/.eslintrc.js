32ed421e98f263bdeb1ec33d0e0f014646e3cb72
Shivanshu840 <rajshivanshu544@gmail.com>
2024-11-18 12:12:30 +0530

    fixing again the error or prisma

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -6,4 +6,14 @@ module.exports = {
   parserOptions: {
     project: true,
   },
+  rules: {
+    "no-unused-vars": ["warn", { 
+      "argsIgnorePattern": "^_",
+      "varsIgnorePattern": "^_"
+    }],
+    "@typescript-eslint/no-unused-vars": ["warn", { 
+      "argsIgnorePattern": "^_",
+      "varsIgnorePattern": "^_"
+    }]
+  }
 };
--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -19,9 +19,7 @@ const nextConfig = {
     env: {
       NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
     },
-    experimental: {
-      serverActions: true,
-    },
+    
   
   }
   
